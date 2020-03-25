b7fe14e81db1d71f758e6984d580981c4850a497
Roman Voknil <roman.voknil@gmail.com>
2020-03-26 00:22:11 +0300

    Развернуть полностью новый проект с нуля в докере. https://trello.com/c/23HQJxOQ

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
