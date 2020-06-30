007f9b3ebd8fb12c7f007b6370869d11ef42f50d
Danil Kuznetsov <danil.kuznetsov23@yandex.r>
2020-07-01 00:15:26 +0300

    add cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
