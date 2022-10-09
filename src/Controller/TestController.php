3484c50cc4f338fa52a3e196bfae4610b4b5acae
Roman Linkov <roman.voknil@gmail.com>
2022-10-09 17:50:33 +0300

    chore: Swagger and routes

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,31 +2,22 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
-use OpenApi\Attributes as OA;
 
 class TestController extends AbstractController
 {
+    #[Route('/api/v1/hello', name: 'api.hello', methods: ['GET'])]
     #[OA\Response(
         response: 200,
-        description: 'Returns the rewards of an user',
-        content: new OA\JsonContent(
-            type: 'array',
-            items: new OA\Items(ref: new Model(type: User::class, groups: ['full']))
-        )
+        description: 'Returns the rewards of an user'
     )]
-    #[OA\Tag(name: 'rewards')]
-    #[Security(name: 'Bearer')]
-    #[Route('/api/v1/test', name: 'app_test')]
-    public function index(): JsonResponse
+    public function test(): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Welcome to your new hello controller!',
             'path' => 'src/Controller/TestController.php',
         ]);
     }
