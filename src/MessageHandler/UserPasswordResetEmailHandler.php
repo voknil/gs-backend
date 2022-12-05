66adbbe1a29ad422f3f4d5ec0f42bbab17249be5
Roman Linkov <roman.voknil@gmail.com>
2022-12-05 16:49:13 +0300

    fix: reset password verify link

--- src/MessageHandler/UserPasswordResetEmailHandler.php
+++ src/MessageHandler/UserPasswordResetEmailHandler.php
@@ -9,27 +9,50 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Address;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 
 #[AsMessageHandler]
 class UserPasswordResetEmailHandler
 {
+    private const URL_SCHEMA = 'https';
+    private const RESET_PASSWORD_VERIFY = '/reset-password-verify';
+
     public function __construct(
         private readonly MailerInterface $mailer,
+        private readonly string $serverName,
     )
     {
     }
 
     public function __invoke(UserPasswordResetEmail $message): void
     {
+        $token = $message->getResetPasswordToken();
+
         $email = (new TemplatedEmail())
             ->from(new Address('no-reply@goodsurfing.org', 'GoodSurfing Team'))
             ->to($message->getEmailTo())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
-                'resetToken' => $message->getResetPasswordToken(),
+                'webUrl' => $this->getUrl($token),
+                'resetToken' => $token,
             ]);
 
         $this->mailer->send($email);
+    }
+
+    private function getUrl(ResetPasswordToken $token): string
+    {
+        // used for split 'localhost, caddy:80'
+        $serverName = explode(',',$this->serverName);
+        $serverName = $serverName[0] ?? '';
+
+        return sprintf(
+            '%s://%s/%s?token=%s',
+            self::URL_SCHEMA,
+            $serverName,
+            self::RESET_PASSWORD_VERIFY,
+            $token->getToken(),
+        );
     }
 }
