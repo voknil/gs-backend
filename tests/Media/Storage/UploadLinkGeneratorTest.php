ce6c58550b012d26d5ae392f7fdf4b5fcd3a4f8b
Roman Linkov <roman.voknil@gmail.com>
2022-12-30 21:11:44 +0300

    chore: phpunit install

---
+++ tests/Media/Storage/UploadLinkGeneratorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Media\Storage;
+
+use PHPUnit\Framework\TestCase;
+
+class UploadLinkGeneratorTest extends TestCase
+{
+    public function testGenerate(): void
+    {
+        $this->assertTrue(true);
+    }
+}
