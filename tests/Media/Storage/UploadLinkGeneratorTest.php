57a8637525e69ae010c76a895e3a9030a830e5d3
Roman Linkov <roman.voknil@gmail.com>
2023-01-06 22:02:26 +0300

    chore: unit test UploadLinkGenerator

--- tests/Media/Storage/UploadLinkGeneratorTest.php
+++ tests/Media/Storage/UploadLinkGeneratorTest.php
@@ -2,12 +2,42 @@
 
 namespace App\Tests\Media\Storage;
 
+use App\Exception\MediaException;
+use App\Media\Storage\GenerateUploadLink;
+use App\Media\Storage\S3\UploadException;
+use App\Media\Storage\Storage;
+use App\Media\Storage\UploadLink;
+use App\Media\Storage\UploadLinkGenerator;
 use PHPUnit\Framework\TestCase;
 
 class UploadLinkGeneratorTest extends TestCase
 {
-    public function testGenerate(): void
+    public function testGenerateSuccess(): void
     {
-        $this->assertTrue(true);
+        $storage = $this->createMock(Storage::class);
+        $storage->expects($this->once())
+            ->method('generateUploadLink')
+            ->willReturn($this->createMock(UploadLink::class));
+
+        $uploadLinkGenerator = new UploadLinkGenerator($storage);
+
+        $command = $this->createStub(GenerateUploadLink::class);
+
+        $uploadLinkGenerator->generate($command);
+    }
+
+    public function testGenerateFail(): void
+    {
+        $storage = $this->createMock(Storage::class);
+        $storage->expects($this->once())
+            ->method('generateUploadLink')
+            ->willThrowException(new UploadException());
+
+        $uploadLinkGenerator = new UploadLinkGenerator($storage);
+
+        $command = $this->createStub(GenerateUploadLink::class);
+
+        $this->expectException(MediaException::class);
+        $uploadLinkGenerator->generate($command);
     }
 }
