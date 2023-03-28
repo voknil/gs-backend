7183b6354f038742325442ffbbb032826a8a6ab1
Roman Linkov <voknil@users.noreply.github.com>
2023-03-28 13:09:58 +0300

    feature: add fields to organization (#53)
    * feature: add fields to organization
    
    * feature: http request for new fields

--- src/Organization/Command/CreateOrganization.php
+++ src/Organization/Command/CreateOrganization.php
@@ -6,5 +6,19 @@ interface CreateOrganization
 {
     public function getName(): string;
 
+    public function getAddress(): ?string;
+
+    public function getType(): ?string;
+
+    public function getWebsite(): ?string;
+
     public function getDescription(): ?string;
+
+    public function getVk(): ?string;
+
+    public function getFacebook(): ?string;
+
+    public function getInstagram(): ?string;
+
+    public function getTelegram(): ?string;
 }
