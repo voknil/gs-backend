5ec4a4817540ec3b731740e53513f72feaf3cb78
devYarar <ismoilov92@gmail.com>
2023-04-20 12:21:09 +0300

    rename table

--- migrations/Version20230419140650.php
+++ migrations/Version20230419140650.php
@@ -20,15 +20,15 @@ final class Version20230419140650 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE photo_gallery (id UUID NOT NULL, id_media_file UUID NOT NULL, id_organization UUID NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_C8A281D34584665A ON photo_gallery (id_organization)');
-        $this->addSql('ALTER TABLE photo_gallery ADD CONSTRAINT FK_C8A281D3F5B7AF75 FOREIGN KEY (id_media_file) REFERENCES media_file (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE photo_gallery ADD CONSTRAINT FK_C8A281D34584665A FOREIGN KEY (id_organization) REFERENCES organization (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE TABLE organization_photo_gallery (id UUID NOT NULL, id_media_file UUID NOT NULL, id_organization UUID NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_C8A281D34584665A ON organization_photo_gallery (id_organization)');
+        $this->addSql('ALTER TABLE organization_photo_gallery ADD CONSTRAINT FK_C8A281D3F5B7AF75 FOREIGN KEY (id_media_file) REFERENCES media_file (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE organization_photo_gallery ADD CONSTRAINT FK_C8A281D34584665A FOREIGN KEY (id_organization) REFERENCES organization (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE photo_gallery');
+        $this->addSql('DROP TABLE organization_photo_gallery');
     }
 }
