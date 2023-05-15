b5dbc427287ce19c3ebc645886865a7ea4a3230a
devYarar <ismoilov92@gmail.com>
2023-05-15 14:06:06 +0300

    test:search user by email

---
+++ tests/User/QueryProcessorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Tests\User;
+
+use App\Entity\User;
+use App\Exception\UserNotFound;
+use App\Persistence\Repository\UserRepository;
+use App\Request\User\Request;
+use App\Response\User\UserForSelect;
+use App\User\QueryProcessor;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\Uuid;
+
+class QueryProcessorTest extends TestCase
+{
+
+    public function testSearch()
+    {
+        $user = $this->createUser('1ed7a121-ad2c-6e28-9d78-7517eeb5c166', 'testname', 'testlastname', 'test@example.com');
+
+        $userRepository = $this->createMock(UserRepository::class);
+        $userRepository->expects($this->once())
+            ->method('getByEmail')
+            ->with('test@example.com')
+            ->willReturn($user);
+
+        $request = $this->createMock(Request::class);
+
+        $request->expects($this->once())
+            ->method('getEmail')
+            ->willReturn('test@example.com');
+
+        $processor = new QueryProcessor($userRepository);
+
+        $expected = new UserForSelect($user);
+
+        $this->assertEquals($expected, $processor->search($request));
+    }
+
+    public function testSearchUserNotFound()
+    {
+
+        $userRepository = $this->createMock(UserRepository::class);
+        $userRepository->expects($this->once())
+            ->method('getByEmail')
+            ->with('nonexistent@example.com')
+            ->willReturn(null);
+
+        $request = $this->createMock(Request::class);
+
+        $request->expects($this->once())
+            ->method('getEmail')
+            ->willReturn('nonexistent@example.com');
+
+        $processor = new QueryProcessor($userRepository);
+
+        $this->expectException(UserNotFound::class);
+
+        $processor->search($request);
+    }
+
+    private function createUser(string $id, string $firstName, string $lastName, string $email): User
+    {
+        return new User(
+            Uuid::fromString($id),
+            $firstName,
+            $lastName,
+            $email
+        );
+    }
+}
