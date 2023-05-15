f1f7cfe95d995711a232f969cd591e7a3d48aaf2
devYarar <ismoilov92@gmail.com>
2023-05-15 15:43:12 +0300

    test:organization user list

---
+++ tests/Organization/QueryProcessorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Tests\Organization;
+
+use App\Entity\Organization;
+use App\Exception\OrganizationNotFound;
+use App\Organization\QueryProcessor;
+use App\Repository\OrganizationRepository;
+use App\Response\Organization\OrganizationUserList;
+use Doctrine\Common\Collections\Collection;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\Uuid;
+
+class QueryProcessorTest extends TestCase
+{
+
+    private function createValidUuid(): Uuid
+    {
+        return Uuid::fromString('1eddcf12-683d-60c2-a04f-554aa5c08e6b');
+    }
+
+    public function testGetUsers()
+    {
+        $uuid = $this->createValidUuid();
+
+        $users = $this->createMock(Collection::class);
+
+        $organization = $this->createMock(Organization::class);
+        $organization->expects($this->once())
+            ->method('getUsers')
+            ->willReturn($users);
+
+        $organizationRepository = $this->createMock(OrganizationRepository::class);
+        $organizationRepository->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo($uuid))
+            ->willReturn($organization);
+
+        $queryProcessor = new QueryProcessor($organizationRepository);
+
+        $result = $queryProcessor->getUsers($uuid);
+
+        $this->assertInstanceOf(OrganizationUserList::class, $result);
+    }
+
+    public function testGetUsersWithInvalidOrganization()
+    {
+        $uuid = $this->createValidUuid();
+
+        $organizationRepository = $this->createMock(OrganizationRepository::class);
+        $organizationRepository->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo($uuid))
+            ->willReturn(null);
+
+        $queryProcessor = new QueryProcessor($organizationRepository);
+
+        $this->expectException(OrganizationNotFound::class);
+
+        $queryProcessor->getUsers($uuid);
+    }
+}
